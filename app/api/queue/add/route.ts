72b880879c0a82977c6fd0b758fc67e2daf0aa7a
Gian Akbar <akbarigi20@gmail.com>
2025-04-13 01:37:21 +0700

    fix : auth

--- app/api/queue/add/route.ts
+++ app/api/queue/add/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 import { addToQueue } from '@/lib/spotify';
 import { supabaseClient } from '@/lib/supabase-client';
 import { getServerSession } from 'next-auth/next';
